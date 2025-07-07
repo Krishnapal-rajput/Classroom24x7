a48a3f826be8a365cb34faf008e38e0dca181518
Krishnapal-rajput <72176093+Krishnapal-rajput@users.noreply.github.com>
2025-07-07 09:11:31 +0530

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -98,7 +98,7 @@ const AdminPanel = () => {
     const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-    XLSX.writeFile(workbook, ${fileName}.xlsx);
+    XLSX.writeFile(workbook, `${fileName}.xlsx`);
   };
 
   // Add employee
